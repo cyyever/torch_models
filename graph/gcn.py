b0eb69885855eea9b38ce02282a250896a5daace
cyy <cyyever@outlook.com>
2023-08-12 15:44:34 +0800

    fix code

--- graph/gcn.py
+++ graph/gcn.py
@@ -26,14 +26,14 @@ class TwoGCN(torch.nn.Module):
     def __init__(self, num_features, num_classes):
         super().__init__()
         self.conv1 = GCNConv(num_features, 1024)
-        self.fc1 = Linear(1024, 512)
+        self.conv2 = GCNConv(1024, 512)
         self.fc2 = Linear(512, 128)
         self.classifier = Linear(128, num_classes)
 
     def forward(self, x, edge_index):
         h = self.conv1(x, edge_index)
         h = h.relu()
-        h = self.fc1(h)
+        h = self.conv2(h, edge_index)
         h = h.relu()
         h = self.fc2(h)
         h = h.relu()
