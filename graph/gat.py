83ef2ca66f38c14c60a93d2034f8a022ade737e8
cyy <cyyever@outlook.com>
2023-08-29 10:36:17 +0800

    fix code

---
+++ graph/gat.py
@@ -0,0 +1,57 @@
+import torch.nn
+from torch.nn import Linear
+from torch_geometric.nn import GATv2Conv
+
+
+class ThreeGCN(torch.nn.Module):
+    def __init__(self, num_features, num_classes):
+        super().__init__()
+        self.conv1 = GATv2Conv(num_features, 1024)
+        self.conv2 = GATv2Conv(1024, 512)
+        self.conv3 = GATv2Conv(512, 128)
+        self.classifier = Linear(128, num_classes)
+
+    def forward(self, x, edge_index, **kwargs):
+        h = self.conv1(x, edge_index)
+        h = h.relu()
+        h = self.conv2(h, edge_index)
+        h = h.relu()
+        h = self.conv3(h, edge_index)
+        h = h.relu()  # Final GNN embedding space.
+        # Apply a final (linear) classifier.
+        return self.classifier(h)
+
+
+class TwoGCN(torch.nn.Module):
+    def __init__(self, num_features, num_classes):
+        super().__init__()
+        self.conv1 = GATv2Conv(num_features, 256)
+        self.conv2 = GATv2Conv(256, 128)
+        self.classifier = Linear(128, num_classes)
+
+    def forward(self, x, edge_index, **kwargs):
+        h = self.conv1(x, edge_index)
+        h = h.relu()
+        h = self.conv2(h, edge_index)
+        h = h.relu()
+        # Apply a final (linear) classifier.
+        return self.classifier(h)
+
+
+class OneGCN(torch.nn.Module):
+    def __init__(self, num_features, num_classes):
+        super().__init__()
+        self.conv1 = GATv2Conv(num_features, 1024)
+        self.fc1 = Linear(1024, 512)
+        self.fc2 = Linear(512, 128)
+        self.classifier = Linear(128, num_classes)
+
+    def forward(self, x, edge_index, **kwargs):
+        h = self.conv1(x, edge_index)
+        h = h.relu()
+        h = self.fc1(h)
+        h = h.relu()
+        h = self.fc2(h)
+        h = h.relu()
+        # Apply a final (linear) classifier.
+        return self.classifier(h)
